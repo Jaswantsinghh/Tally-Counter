8f13fc4bf6d4f1dc93098adba8f71afd5b7fdd0d
Jaswant Singh <54163759+Jaswantsinghh@users.noreply.github.com>
2021-04-24 13:52:16 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+var counter;
+if(counter='undefined') counter =0;
+
+var counter = localStorage.getItem("myval");
+document.getElementById('heading').innerHTML=counter;
+function fun()
+{
+    ++counter;
+    localStorage.setItem("myval",counter);
+    document.getElementById('heading').innerHTML=counter;
+}
+
+function dec()
+{
+    
+    if(counter==0) counter=0;
+    else
+    {
+        --counter;
+    }
+    local();
+    document.getElementById('heading').innerHTML=counter;
+}
+
+function reset()
+{
+    counter = 0;
+    local();
+    document.getElementById('heading').innerHTML=counter;
+}
+
+function local()
+{
+    localStorage.setItem("myval",counter);
+}
+
+function save()
+{
+    
+}
