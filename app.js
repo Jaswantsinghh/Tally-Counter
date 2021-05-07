29f178746a116987809aa0dbe3ae426067294d5b
Jaswant Singh <54163759+Jaswantsinghh@users.noreply.github.com>
2021-05-07 21:38:15 +0530

    bug fixed

--- app.js
+++ app.js
@@ -1,40 +1,37 @@
-var counter;
-if(counter='undefined') counter =0;
+if (counter = 'NaN') counter = 0;
+if (counter = 'undefined') counter = 0;
 
 var counter = localStorage.getItem("myval");
-document.getElementById('heading').innerHTML=counter;
-function fun()
-{
+document.getElementById('heading').innerHTML = counter;
+function fun() {
     ++counter;
-    localStorage.setItem("myval",counter);
-    document.getElementById('heading').innerHTML=counter;
+    localStorage.setItem("myval", counter);
+    document.getElementById('heading').innerHTML = counter;
 }
 
-function dec()
-{
-    
-    if(counter<=0) counter=0;
-    else
-    {
+function dec() {
+
+    if (counter <= 0) counter = 0;
+    else {
         --counter;
     }
     local();
-    document.getElementById('heading').innerHTML=counter;
+    document.getElementById('heading').innerHTML = counter;
 }
 
-function reset()
-{
+function reset() {
     counter = 0;
     local();
-    document.getElementById('heading').innerHTML=counter;
+    document.getElementById('heading').innerHTML = counter;
 }
 
-function local()
-{
-    localStorage.setItem("myval","counter");
+function local() {
+    localStorage.setItem("myval", counter);
 }
 
-function save()
-{
-    
+function save() {
+    var btn = document.createElement("Button");
+
+    btn.innerHTML = "Save It";
+    document.body.appendChild(btn);
 }
